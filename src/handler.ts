e382f3482fbd1ddc9f883dd46875de73efe1ecac
Chris <cbhubbard8@gmail.com>
2020-01-05 15:37:59 -0600

    Update to work with windows and have default

--- src/handler.ts
+++ src/handler.ts
@@ -1,24 +1,16 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context, Handler } from 'aws-lambda';
-import { HTTP_STATUS_CODES } from './constants';
 import * as envConfigs from './config/env.config.json';
+import { HTTP_STATUS_CODES } from './constants';
 
 export const getConfigForEnv: Handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
     console.log(event);
-    if (!event.queryStringParameters)
-    {
-        return {
-            statusCode: HTTP_STATUS_CODES.CLIENT_ERROR,
-            body: JSON.stringify({
-                message: 'No query parameters were passed'
-            })
-        };
-    }
+    const { env } = Object(event.queryStringParameters);
 
-    const { env } = event.queryStringParameters;
+    // Use the passed config or by default the config that this lambda is running in
     return {
         statusCode: HTTP_STATUS_CODES.SUCCESS,
         body: JSON.stringify({
-            config: envConfigs[env.toUpperCase()]
+            config: envConfigs[(env || (process.env.ENV as string)).toUpperCase()]
         })
     };
 };
